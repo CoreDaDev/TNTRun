31121407ae0b2860d6466031fbbf745ff323a505
Oğuzhan <64753457+OguzhanUmutlu@users.noreply.github.com>
2021-03-29 18:58:53 +0300

    another fixxxx

--- src/OguzhanUmutlu/TNTRun/commands/TNTRunCommand.php
+++ src/OguzhanUmutlu/TNTRun/commands/TNTRunCommand.php
@@ -112,7 +112,7 @@ class TNTRunCommand extends Command implements PluginIdentifiableCommand {
                     $sender->sendMessage("§c> $args[1] named arena not found!");
                     break;
                 }
-                $this->plugin->arenas[$index]->setup = false;
+                $this->plugin->arenas[$index]->setup = true;
                 $sender->sendMessage("§6> You are in setup mode.\n".
                     "§7- Use §lhelp §r§7to see commands\n"  .
                     "§7- or use §ldone §r§7to leave from setup mode");
